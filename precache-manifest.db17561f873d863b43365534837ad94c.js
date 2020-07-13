1b82c6ef8958eedd8b528eb93d3f3e3cd8383c47
elenawq <60143171+elenawq@users.noreply.github.com>
2020-07-13 17:24:48 +0300

    Updates

---
+++ precache-manifest.db17561f873d863b43365534837ad94c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "889773a7e178f3692a04a4bf3fb44cd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f88c1366fd9af14ae098",
+    "url": "/static/css/main.6661cb28.chunk.css"
+  },
+  {
+    "revision": "80e76b82588292b5ea99",
+    "url": "/static/js/2.6e4418f2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e4418f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f88c1366fd9af14ae098",
+    "url": "/static/js/main.5f967237.chunk.js"
+  },
+  {
+    "revision": "5738b0f7c428695a68f5",
+    "url": "/static/js/runtime-main.2e17527d.js"
+  }
+]);
